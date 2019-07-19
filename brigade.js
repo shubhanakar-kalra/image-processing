7d5689235a00851f3bcdc5ee2f05fa2df89ab535
idadmin <idadmin@lapr169.successivetech.com>
2019-07-19 14:27:44 +0530

    lint check

--- brigade.js
+++ brigade.js
@@ -1,6 +1,12 @@
-const { events }  = require("brigadier")
+const { events, Job }  = require("brigadier")
 
 events.on("push", () => {
-  console.log("==> handling an 'push' event hello to tdajkdnald")
+  console.log("==> handling an 'push' event hello to bald")
 
+  var job = new Job("lint-check", "eslint:^6.0.1")
+
+  job.tasks = [
+    "npm i",
+    "echo lint checked"
+  ]
 })
