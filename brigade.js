3a0201e13a15f6dccbea8044f5c415ca0d273aaf
idadmin <idadmin@lapr169.successivetech.com>
2019-07-19 16:15:13 +0530

    add

--- brigade.js
+++ brigade.js
@@ -3,11 +3,11 @@ const { events, Job }  = require("brigadier")
 events.on("push", () => {
   console.log("==> handling an 'push' event hello to bald")
 
-  var job = new Job("lint-check", "eslint:^6.0.1")
+  var job = new Job("lint-check", "alpine:3.4")
 
   job.tasks = [
     "npm i",
-    "echo lint checked"
+    "eslint --fix ."
   ]
   job.run()
 })
