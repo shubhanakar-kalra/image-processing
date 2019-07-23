34bab567c74432275d6dfe9b773a45d5119caadc
idadmin <idadmin@lapr169.successivetech.com>
2019-07-23 17:41:15 +0530

    Stratus add

--- brigade.js
+++ brigade.js
@@ -1,21 +1,24 @@
-const { events, Job }  = require("brigadier")
+const { events, Job , Group}  = require("brigadier")
 
 events.on("push", () => {
   console.log("==> handling an 'push' event hello to bald")
-
-  var job = new Job("lint-check", "node:8")
+  let group = new Group();
+  let job = new Job("lint-check", "node:8")
 
   job.tasks = [
     "cd src/",
     "npm i"
       ]
-  job.run()
+  //job.run()
+  group.add(job);
   console.log("Job successfuly ran")
-  var job2 = new Job("Dockerization", "docker:18.09")
+  let job2 = new Job("Dockerization", "docker:18.09")
 
   job2.tasks = [
     "workdir /app",
     "npm i"
   ]
-  job2.run()
+  group.add(job2);
+  //job2.run()
+
 })
