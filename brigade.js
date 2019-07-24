12fd2266777f4db02798c72f21062a71db7da9e6
idadmin <idadmin@lapr169.successivetech.com>
2019-07-24 11:40:41 +0530

    add

--- brigade.js
+++ brigade.js
@@ -1,14 +1,14 @@
-const { events, Job }  = require("brigadier")
+const { events, Job, Group }  = require("brigadier")
 
 events.on("push", () => {
   console.log("==> handling an 'push' event hello to bald")
-  //let group = new Group();
-  //let job = new Job("lint-check", "node:8")
+  let group = new Group();
+  let job = new Job("lint-check", "node:8")
 
-  //job.tasks = [
-   // "cd src/",
-//"npm i"
-    //  ]
+  job.tasks = [
+   "cd src/",
+    "npm i"
+      ]
  
   
   let job2 = new Job("dockerization", "docker:stable-dind")
@@ -16,8 +16,8 @@ events.on("push", () => {
   job2.tasks = [
     "mkdir /app",
     "ls -lart",
-    "docker --help "
+    "docker --help ",
   ]
- job2.run();
-
+  job2.run();
+  group.add(job,job2);
 })
