82469238f0075c558b3386548a906e4313b0be28
idadmin <idadmin@lapr169.successivetech.com>
2019-07-24 16:43:16 +0530

    add

--- brigade.js
+++ brigade.js
@@ -9,16 +9,17 @@ events.on("push", () => {
    //"cd src/",
     //"npm i"
      // ]
- 
-  
   let job2 = new Job("dockerization", "docker:stable-dind")
-
+  job2.privileged = true;
+  job2.env = {
+  DOCKER_DRIVER: "overlay"
+}
   job2.tasks = [
     "mkdir /app",
     "cd src",
     "ls -lart",
     "dockerd",
-    "docker build -t helloworld . ",
+    "docker build shaxxz13/imageprocess:v2 . ",
 
   ]
   job2.run();
