864ac8d053ff33ec53f68eedf3ba0a5ff0997bac
idadmin <idadmin@lapr169.successivetech.com>
2019-07-29 12:15:40 +0530

    add

--- brigade.js
+++ brigade.js
@@ -26,7 +26,8 @@ events.on("push", () => {
     //"docker build -t shaxxz13/shubhuxx . ",
     "docker pull google/cloud-sdk:latest",
     "docker run google/cloud-sdk:latest gcloud version",
-    "gcloud auth activate-service-account --key-file=keys.json"
+    "gcloud -v"
+    //"gcloud auth activate-service-account --key-file=keys.json"
    // "docker login -u shaxxz13 -p shubhu9797",
     //"docker push shaxxz13/shubhuxx"
   ]
