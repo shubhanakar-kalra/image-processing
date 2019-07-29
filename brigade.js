f20026829d231ffb6e731956bf2906f8a7e87673
idadmin <idadmin@lapr169.successivetech.com>
2019-07-29 14:04:58 +0530

    add

--- brigade.js
+++ brigade.js
@@ -10,7 +10,7 @@ events.on("push", () => {
     "npm i"
 ]
 
-  let job2 = new Job("dockerization", "docker:stable-dind")
+  let job2 = new Job("docker", "docker:stable-dind")
   job2.privileged = true;
   job2.env = {
     DOCKER_DRIVER: "overlay"
@@ -23,19 +23,18 @@ events.on("push", () => {
     "dockerd-entrypoint.sh &",
     "sleep 10",
     "export SKIP_PREFLIGHT_CHECK=true",
-    "curl https://dl.google.com/dl/cloudsdk/release/google-cloud-sdk.tar.gz > /tmp/google-cloud-sdk.tar.gz"
+    "wget https://dl.google.com/dl/cloudsdk/release/google-cloud-sdk.tar.gz"
     //"docker build -t shaxxz13/shubhuxx . ",
     //"docker pull google/cloud-sdk:latest",
     //"docker run google/cloud-sdk:latest gcloud version",
     //"gcloud auth activate-service-account --key-file=keys.json"
-   //"docker login -u shaxxz13 -p shubhu9797",
-  //"docker push shaxxz13/shubhuxx"
+    //"docker login -u shaxxz13 -p shubhu9797",
+    //"docker push shaxxz13/shubhuxx"
   ]
   //job.run().then(() => {
     job2.run()
   //})
-  
-//hello
+
   
 
 
