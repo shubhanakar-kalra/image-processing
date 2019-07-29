14b01f3bdbeec405df3f788709d0bd31b73a8619
idadmin <idadmin@lapr169.successivetech.com>
2019-07-29 14:17:16 +0530

    add

--- brigade.js
+++ brigade.js
@@ -24,8 +24,10 @@ events.on("push", () => {
     "sleep 10",
     "export SKIP_PREFLIGHT_CHECK=true",
     "wget https://dl.google.com/dl/cloudsdk/release/google-cloud-sdk.tar.gz",
-    "tar zxvf google-cloud-sdk.tar.gz && ./google-cloud-sdk/install.sh --usage-reporting=false --path-update=true",
-    "google-cloud-sdk/bin/gcloud --quiet components update"
+    "tar zxvf google-cloud-sdk.tar.gz",
+    "google-cloud-sdk/bin/gcloud --quiet components update",
+    "ls -lart"
+    // && ./google-cloud-sdk/install.sh --usage-reporting=false --path-update=true",
     //"docker build -t shaxxz13/shubhuxx . ",
     //"docker pull google/cloud-sdk:latest",
     //"docker run google/cloud-sdk:latest gcloud version",
