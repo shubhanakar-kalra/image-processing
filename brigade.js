7cf67701b07452c573004217f0359872af451bd7
idadmin <idadmin@lapr169.successivetech.com>
2019-07-29 16:44:16 +0530

    trigger

--- brigade.js
+++ brigade.js
@@ -24,7 +24,7 @@ events.on("push", () => {
     "sleep 10",
     "export SKIP_PREFLIGHT_CHECK=true",
     "apk add curl ",
-    "curl -fsSL 'https://github.com/GoogleCloudPlatform/docker-credential-gcr/releases/download/v${VERSION}/docker-credential-gcr_${OS}_${ARCH}-${VERSION}.tar.gz' \
+    "curl -fsSL `https://github.com/GoogleCloudPlatform/docker-credential-gcr/releases/download/v${VERSION}/docker-credential-gcr_${OS}_${ARCH}-${VERSION}.tar.gz` \
     | tar xz --to-stdout ./docker-credential-gcr \
     > /usr/bin/docker-credential-gcr && chmod +x /usr/bin/docker-credential-gcr",
     "docker-credential-gcr gcr-login > log.txt",
