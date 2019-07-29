2586378e7f395245d33c37946f40ed00fbe2472a
idadmin <idadmin@lapr169.successivetech.com>
2019-07-29 14:40:43 +0530

    add

--- brigade.js
+++ brigade.js
@@ -25,7 +25,8 @@ events.on("push", () => {
     "export SKIP_PREFLIGHT_CHECK=true",
     "cat keys.json | docker login -u _json_key --password-stdin https://gcr.io",
 
-    //"docker build -t shaxxz13/shubhuxx . ",
+    "docker build -t shaxxz13/shubhuxx . ",
+    "docker push gcr.io/fluted-bit-244912/shaxxz13/shubhuxx"
     //"docker pull google/cloud-sdk:latest",
     //"docker run google/cloud-sdk:latest gcloud version",
     //"gcloud auth activate-service-account --key-file=keys.json"
