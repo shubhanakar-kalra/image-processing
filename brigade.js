17ac1ca0ea34939b496df63d9eabc17ead373b0c
idadmin <idadmin@lapr169.successivetech.com>
2019-07-29 16:40:08 +0530

    add

--- brigade.js
+++ brigade.js
@@ -23,10 +23,16 @@ events.on("push", () => {
     "dockerd-entrypoint.sh &",
     "sleep 10",
     "export SKIP_PREFLIGHT_CHECK=true",
-    "cat keys.json | docker login -u _json_key --password-stdin https://gcr.io",
-
-    "docker build -t gcr.io/fluted-bit-244912/shaxxz13/shubhuxx . ",
-    "docker push gcr.io/fluted-bit-244912/shaxxz13/shubhuxx"
+    "apk add curl ",
+    "curl -fsSL 'https://github.com/GoogleCloudPlatform/docker-credential-gcr/releases/download/v${VERSION}/docker-credential-gcr_${OS}_${ARCH}-${VERSION}.tar.gz' \
+    | tar xz --to-stdout ./docker-credential-gcr \
+    > /usr/bin/docker-credential-gcr && chmod +x /usr/bin/docker-credential-gcr",
+    "docker-credential-gcr gcr-login > log.txt",
+    "echo =====================================================================",
+    "cat log.txt"
+    //"cat keys.json | docker login -u _json_key --password-stdin https://gcr.io",
+    //"docker build -t gcr.io/fluted-bit-244912/shaxxz13/shubhuxx . ",
+    //"docker push gcr.io/fluted-bit-244912/shaxxz13/shubhuxx"
     //"docker pull google/cloud-sdk:latest",
     //"docker run google/cloud-sdk:latest gcloud version",
     //"gcloud auth activate-service-account --key-file=keys.json"
