ad3f4cd836d9bdd20b431f4549b34b09ed27189b
idadmin <idadmin@lapr169.successivetech.com>
2019-07-30 16:16:28 +0530

    add

--- brigade.js
+++ brigade.js
@@ -18,7 +18,7 @@ events.on("push", (project) => {
   }
    job2.env = 
     {
-  "TYPE": project.secrets.type,
+  //"TYPE": project.secrets.type,
   "PROJECT_ID": project.secrets.project_id,
   "PRIVATE_KEY_ID": project.secrets.private_key_id
   // "PRIVATE_KEY": project.secrets.private_key_id,
