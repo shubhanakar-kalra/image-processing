5d10707f0f070f38e68111370b55f2bad45658f6
idadmin <idadmin@lapr169.successivetech.com>
2019-07-30 18:47:10 +0530

    add

--- brigade.js
+++ brigade.js
@@ -23,7 +23,7 @@ events.on("push", (_, project) => {
     AUTH_URI: project.secrets.auth_uri,
     TOKEN_URI: project.secrets.token_uri,
     AUTH_PROVIDER_X509_CERT_URL: project.secrets.auth_provider_x509_cert_url,
-    CLIENT_X509_CERT_URL: project.secrets.CLIENT_X509_CERT_URL
+    CLIENT_X509_CERT_URL: project.secrets.client_x509_cert_url
   }
   job2.tasks = [
     "cd src",
