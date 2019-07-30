3f13eff97deaf355559da0a4a68efd08f5975a66
idadmin <idadmin@lapr169.successivetech.com>
2019-07-30 18:34:49 +0530

    add

--- brigade.js
+++ brigade.js
@@ -15,21 +15,22 @@ events.on("push", (_, project) => {
   job2.env = {
     DOCKER_DRIVER: "overlay",
     TYPE: project.secrets.type,
-    // "PROJECT_ID": project.secrets.project_id,
-    // "PRIVATE_KEY_ID": project.secrets.private_key_id,
-    // "PRIVATE_KEY": project.secrets.private_key_id,
-    // "CLIENT_EMAIL": project.secrets.client_email,
-    // "CLIENT_ID": project.secrets.client_id,
-    // "AUTH_URI": project.secrets.auth_uri,
-    // "TOKEN_URI": project.secrets.token_uri,
-    // "AUTH_PROVIDER_X509_CERT_URL": project.secrets.auth_provider_x509_cert_url,
-    // "CLIENT_X509_CERT_URL": project.secrets.CLIENT_X509_CERT_URL
+    PROJECT_ID: project.secrets.project_id,
+    PRIVATE_KEY_ID: project.secrets.private_key_id,
+    PRIVATE_KEY: project.secrets.private_key_id,
+    CLIENT_EMAIL: project.secrets.client_email,
+    CLIENT_ID: project.secrets.client_id,
+    AUTH_URI: project.secrets.auth_uri,
+    TOKEN_URI: project.secrets.token_uri,
+    AUTH_PROVIDER_X509_CERT_URL: project.secrets.auth_provider_x509_cert_url,
+    CLIENT_X509_CERT_URL: project.secrets.CLIENT_X509_CERT_URL
   }
-
+  let envobj = JSON.stringify(job2.env)
   job2.tasks = [
     "cd src",
     "ls -lart",
     "echo $TYPE",
+    `echo ${envobj}`,
     `echo ${project.secrets.type}`
     // "dockerd-entrypoint.sh &",
     // "sleep 10",
