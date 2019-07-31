5a7edc2d9971102fe21f3dba9ae57f1a964b4c41
idadmin <idadmin@lapr169.successivetech.com>
2019-07-31 13:55:03 +0530

    add

--- brigade.js
+++ brigade.js
@@ -35,7 +35,7 @@ events.on("push", (_, project) => {
     "dockerd-entrypoint.sh &",
     "sleep 10",
     "export SKIP_PREFLIGHT_CHECK=true",
-    "echo '{type: ${TYPE}, project_id: ${PROJECT_ID}, private_key_id: ${PRIVATE_KEY_ID}, private_key: ${PRIVATE_KEY}, client_email: ${CLIENT_EMAIL}, client_id: ${CLIENT_ID}, auth_uri: ${AUTH_URI}, auth_provider_x509_cert_url: ${AUTH_PROVIDER_X509_CERT_URL}, client_x509_cert_url: ${CLIENT_X509_CERT_URL} }' > cat key.json",
+    "echo {type: ${TYPE}, project_id: ${PROJECT_ID}, private_key_id: ${PRIVATE_KEY_ID}, private_key: ${PRIVATE_KEY}, client_email: ${CLIENT_EMAIL}, client_id: ${CLIENT_ID}, auth_uri: ${AUTH_URI}, auth_provider_x509_cert_url: ${AUTH_PROVIDER_X509_CERT_URL}, client_x509_cert_url: ${CLIENT_X509_CERT_URL} } > cat key.json",
     "cat key.json | docker login -u _json_key --password-stdin https://gcr.io",
     // "docker build -t gcr.io/fluted-bit-244912/shaxxz13/shubhuxx . ",
     // "docker push gcr.io/fluted-bit-244912/shaxxz13/shubhuxx"
