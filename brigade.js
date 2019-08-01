3f1ac1174420f0b30368277ee1576ea636de96d7
idadmin <idadmin@lapr169.successivetech.com>
2019-08-01 15:48:00 +0530

    add

--- brigade.js
+++ brigade.js
@@ -13,16 +13,16 @@ events.on("push", (_, project) => {
   let job2 = new Job("docker", "docker:stable-dind")
   job2.privileged = true;
   var keysvalue = {
-    TYPE: project.secrets.type,
-    PROJECT_ID: project.secrets.project_id,
-    PRIVATE_KEY_ID: project.secrets.private_key_id,
-    PRIVATE_KEY: project.secrets.private_key,
-    CLIENT_EMAIL: project.secrets.client_email,
-    CLIENT_ID: project.secrets.client_id,
-    AUTH_URI: project.secrets.auth_uri,
-    TOKEN_URI: project.secrets.token_uri,
-    AUTH_PROVIDER_X509_CERT_URL: project.secrets.auth_provider_x509_cert_url,
-    CLIENT_X509_CERT_URL: project.secrets.client_x509_cert_url
+    type: project.secrets.type,
+    project_id: project.secrets.project_id,
+    private_key_id: project.secrets.private_key_id,
+    private_key: project.secrets.private_key,
+    client_email: project.secrets.client_email,
+    client_id: project.secrets.client_id,
+    auth_uri: project.secrets.auth_uri,
+    token_uri: project.secrets.token_uri,
+    auth_provider_x509_cert_url: project.secrets.auth_provider_x509_cert_url,
+    client_x509_cert_url: project.secrets.client_x509_cert_url
   }
 
   job2.env = {
