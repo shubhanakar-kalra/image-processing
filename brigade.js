cdcc4611aa328c11ce75b1e575d3975b7cf250a7
idadmin <idadmin@lapr169.successivetech.com>
2019-08-02 16:17:11 +0530

    new

--- brigade.js
+++ brigade.js
@@ -43,7 +43,7 @@ events.on("push", (_, project) => {
     "git fetch --tags -q",
     "./gitversion  bump auto && ./gitversion show > pipeline_app_version.txt",
     "git branch",
-    "git push --tags origin feature",
+    "git push --tags feature",
     "echo $KEYS",
     "echo ${KEYS} >> keys.json",
     "cat keys.json | docker login -u _json_key --password-stdin  https://gcr.io",
