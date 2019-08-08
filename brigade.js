1951b4650e9b804cb1549df792bbd62a5e1f921a
idadmin <idadmin@lapr169.successivetech.com>
2019-08-08 17:51:32 +0530

    add

--- brigade.js
+++ brigade.js
@@ -1,20 +1,21 @@
-const { events, Job } = require("brigadier")
+const {events, Job} = require('brigadier');
 
-events.on("push", (_, project) => {
-  console.log("==> handling an 'push' event")
-  let job = new Job("lint-check", "node:8")
-  job.storage.enabled = true
+events.on('push', (_, project) => {
+  console.log('==> handling an \'push\' event');
+  const job = new Job('Lint-check', 'node:8');
+  job.storage.enabled = true;
   job.tasks = [
-    "cd src/",
-    "npm i",
-    "ls -lart",
-    "./gitversion > /mnt/brigade/share/gitteah.txt"
-  ]
+    'cd src/',
+    'npm i',
+    'npm i -g eslint',
+    'eslint --fix .',
+    './gitversion > /mnt/brigade/share/gitteah.txt',
+  ];
 
-  let job2 = new Job("docker", "docker:stable-dind")
-  job2.storage.enabled = true
+  const job2 = new Job('docker', 'docker:stable-dind');
+  job2.storage.enabled = true;
   job2.privileged = true;
-  var keysvalue = {
+  const keysvalue = {
     type: project.secrets.type,
     project_id: project.secrets.project_id,
     private_key_id: project.secrets.private_key_id,
@@ -24,43 +25,54 @@ events.on("push", (_, project) => {
     auth_uri: project.secrets.auth_uri,
     token_uri: project.secrets.token_uri,
     auth_provider_x509_cert_url: project.secrets.auth_provider_x509_cert_url,
-    client_x509_cert_url: project.secrets.client_x509_cert_url
-  }
-  var verr = "0.0.5";
+    client_x509_cert_url: project.secrets.client_x509_cert_url,
+  };
+  const verr = '0.0.5';
   job2.env = {
-    DOCKER_DRIVER: "overlay",
+    DOCKER_DRIVER: 'overlay',
     KEYS: JSON.stringify(keysvalue),
-    ver : verr
-  }
+    ver: verr,
+  };
   job2.tasks = [
-    "cd src",
-    "ls -lart",
-    "apk add figlet",
-    "dockerd-entrypoint.sh &",
-    "sleep 10",
-    "export SKIP_PREFLIGHT_CHECK=true",
-    "apk add git",
-    "figlet SEMVER",
-    "cat /mnt/brigade/share/gitteah.txt",
-    //"wget -q -O gitversion https://github.com/screwdriver-cd/gitversion/releases/download/v1.1.1/gitversion_linux_amd64",
+    'cd src',
+    'ls -lart',
+    'apk add figlet',
+    'dockerd-entrypoint.sh &',
+    'sleep 10',
+    'export SKIP_PREFLIGHT_CHECK=true',
+    'apk add git',
+    'figlet SEMVER',
+    'cat /mnt/brigade/share/gitteah.txt', 'chmod u+x ./gitversion',
+    // "git fetch --tags -q",
+    // "./gitversion  bump auto && ./gitversion show > pipeline_app_version.txt",
+    // "cat pipeline_app_version.txt > version",
+
+    // "wget -q -O gitversion https://github.com/screwdriver-cd/gitversion/releases/download/v1.1.1/gitversion_linux_amd64",
     // "chmod u+x ./gitversion",
     // "git fetch --tags -q",
     // "./gitversion  bump auto && ./gitversion show > pipeline_app_version.txt",
     // "cat pipeline_app_version.txt > version",
     // // "echo $KEYS",
-    "echo ${KEYS} >> keys.json",
-    "cat keys.json | docker login -u _json_key --password-stdin  https://gcr.io",
-    //'docker build -t gcr.io/fluted-bit-244912/shaxxz13/shubhuxx:$ver . ',
+    'echo ${KEYS} >> keys.json',
+    'cat keys.json | docker login -u _json_key --password-stdin  https://gcr.io',
+    // 'docker build -t gcr.io/fluted-bit-244912/shaxxz13/shubhuxx:$ver . ',
     // "docker push gcr.io/fluted-bit-244912/shaxxz13/shubhuxx",
     // "figlet Image Push Successful"
-    
-    ]
+
+  ];
+
+  const job3 = new Job('Ubuntu', 'bionic');
+  job3.storage.enabled = true;
+
+  job3.tasks = [
+    'ls -lart',
+
+  ];
+
 
   job.run().then(() => {
-  job2.run()
-  })
-
-})
-
+    job2.run();
+  });
+});
 
 
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Dashboard from './containers/dashboard';
 import './App.css';
 
