dd8ab70067eadad1e93b20fe0c7f8613892f234c
idadmin <idadmin@lapr169.successivetech.com>
2019-08-08 13:56:57 +0530

    add

--- brigade.js
+++ brigade.js
@@ -3,7 +3,7 @@ const { events, Job } = require("brigadier")
 events.on("push", (_, project) => {
   console.log("==> handling an 'push' event")
   let job = new Job("lint-check", "node:8")
-  //job.storage.enabled = true
+  job.storage.enabled = true
   job.tasks = [
     "cd src/",
     // "npm i"
