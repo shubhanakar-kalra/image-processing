d2d5d663e554afe32c458f2cd640d4fe4f982a5d
idadmin <idadmin@lapr169.successivetech.com>
2019-08-09 12:10:12 +0530

    add

--- brigade.js
+++ brigade.js
@@ -1,72 +1,66 @@
-const { events, Job } = require("brigadier")
+const { events, Job }  = require("brigadier")
 
-events.on("push", (_, project) => {
-  console.log("==> handling an 'push' event")
-  let job = new Job("Lint-check", "node:8")
-  job.storage.enabled = true
-  job.tasks = [
-    "cd src/",
-    "npm i",
-    "npm i -g eslint",
-    "./gitversion > /mnt/brigade/share/gitteah.txt"
-  ]
+events.on("push", () => {
+  console.log("==> handling an 'push' event hello to bald")
+ //let group = new Group();
+ let job = new Job("lint-check", "node:8")
+ job.storage.enabled = true
+ job.tasks = [
+   'cd src/',
+   'npm i',
+   'npm i -g eslint',
+   './gitversion > /mnt/brigade/share/gitteah.txt'
+ ]
+ let job2 = new Job("docker", "docker:stable-dind")
+ job2.storage.enabled = true
+ job2.privileged = true;
+ var keysvalue = {
+   type: project.secrets.type,
+   project_id: project.secrets.project_id,
+   private_key_id: project.secrets.private_key_id,
+   private_key: project.secrets.private_key,
+   client_email: project.secrets.client_email,
+   client_id: project.secrets.client_id,
+   auth_uri: project.secrets.auth_uri,
+   token_uri: project.secrets.token_uri,
+   auth_provider_x509_cert_url: project.secrets.auth_provider_x509_cert_url,
+   client_x509_cert_url: project.secrets.client_x509_cert_url
+ }
+ var verr = "0.0.5";
+ job2.env = {
+   DOCKER_DRIVER: "overlay",
+   KEYS: JSON.stringify(keysvalue),
+   ver: verr
+ }
+ job2.tasks = [
+   "cd src",
+   "ls -lart",
+   "apk add figlet",
+   "dockerd-entrypoint.sh &",
+   "sleep 10",
+   "export SKIP_PREFLIGHT_CHECK=true",
+   "apk add git",
+   "figlet SEMVER",
+   "cat /mnt/brigade/share/gitteah.txt",
+   "chmod u+x ./gitversion",
+   // "git fetch --tags -q",
+   // "./gitversion  bump auto && ./gitversion show > pipeline_app_version.txt",
+   // "cat pipeline_app_version.txt > version",
+   //"wget -q -O gitversion https://github.com/screwdriver-cd/gitversion/releases/download/v1.1.1/gitversion_linux_amd64",
+   // "chmod u+x ./gitversion",
+   // "git fetch --tags -q",
+   // "./gitversion  bump auto && ./gitversion show > pipeline_app_version.txt",
+   // "cat pipeline_app_version.txt > version",
+   // // "echo $KEYS",
+   "echo ${KEYS} >> keys.json",
+   "cat keys.json | docker login -u _json_key --password-stdin  https://gcr.io",
+   //'docker build -t gcr.io/fluted-bit-244912/shaxxz13/shubhuxx:$ver . ',
+   // "docker push gcr.io/fluted-bit-244912/shaxxz13/shubhuxx",
+   // "figlet Image Push Successful"
 
-  let job2 = new Job("docker", "docker:stable-dind")
-  job2.storage.enabled = true
-  job2.privileged = true
-  var keysvalue = {
-    type: project.secrets.type,
-    project_id: project.secrets.project_id,
-    private_key_id: project.secrets.private_key_id,
-    private_key: project.secrets.private_key,
-    client_email: project.secrets.client_email,
-    client_id: project.secrets.client_id,
-    auth_uri: project.secrets.auth_uri,
-    token_uri: project.secrets.token_uri,
-    auth_provider_x509_cert_url: project.secrets.auth_provider_x509_cert_url,
-    client_x509_cert_url: project.secrets.client_x509_cert_url
-  }
-  var verr = "0.0.5";
-  job2.env = {
-    DOCKER_DRIVER: "overlay",
-    KEYS: JSON.stringify(keysvalue),
-    ver: verr
-  }
-  job2.tasks = [
-    "cd src",
-    "ls -lart",
-    "apk add figlet",
-    "dockerd-entrypoint.sh &",
-    "sleep 10",
-    "export SKIP_PREFLIGHT_CHECK=true",
-    "apk add git",
-    "figlet SEMVER",
-    "cat /mnt/brigade/share/gitteah.txt", "chmod u+x ./gitversion",
-    // "git fetch --tags -q",
-    // "./gitversion  bump auto && ./gitversion show > pipeline_app_version.txt",
-    // "cat pipeline_app_version.txt > version",
-
-    //"wget -q -O gitversion https://github.com/screwdriver-cd/gitversion/releases/download/v1.1.1/gitversion_linux_amd64",
-    // "chmod u+x ./gitversion",
-    // "git fetch --tags -q",
-    // "./gitversion  bump auto && ./gitversion show > pipeline_app_version.txt",
-    // "cat pipeline_app_version.txt > version",
-    // // "echo $KEYS",
-    "echo ${KEYS} >> keys.json",
-    "cat keys.json | docker login -u _json_key --password-stdin  https://gcr.io",
-    //'docker build -t gcr.io/fluted-bit-244912/shaxxz13/shubhuxx:$ver . ',
-    // "docker push gcr.io/fluted-bit-244912/shaxxz13/shubhuxx",
-    // "figlet Image Push Successful"
-
-  ]
-
-
-
-  job.run().then(() => {
-    job2.run()
-  })
+ ]
+ job.run().then(() => {
+   job2.run()
+ })
 
 })
-
-
-
