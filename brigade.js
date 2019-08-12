dc47e436767b0fd934d6f5b9427d2679787b8a54
idadmin <idadmin@lapr169.successivetech.com>
2019-08-12 17:58:19 +0530

    addnew

--- brigade.js
+++ brigade.js
@@ -73,7 +73,7 @@ job2.tasks = [
     "ls",
     "gcloud config set project fluted-bit-244912",
     "gcloud auth activate-service-account 200715180751-compute@developer.gserviceaccount.com --key-file=/mnt/brigade/share/keys.json --project=fluted-bit-244912",
-
+    "gcloud container clusters get-credentials standard-cluster-1 --zone us-central1-a --project fluted-bit-244912"
     
 
 ]
