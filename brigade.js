2a452ab99a815a412a04404b10b4f6cf531ed544
idadmin <idadmin@lapr169.successivetech.com>
2019-08-12 16:59:09 +0530

    add

--- brigade.js
+++ brigade.js
@@ -74,7 +74,6 @@ job2.tasks = [
     "gcloud auth activate-service-account \
     200715180751-compute@developer.gserviceaccount.com \
     --key-file=/mnt/brigade/share/keys.json --project=fluted-bit-244912",
-    "gcloud container clusters get-credentials",
     "gcloud components install kubectl"
     //"gcloud container clusters get-credentials standard-cluster-1 --zone us-central1-a --project fluted-bit-244912"
     
