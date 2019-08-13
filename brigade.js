eb2f11d65cc501b0e5cd27c086bec4d618b4e9ea
idadmin <idadmin@lapr169.successivetech.com>
2019-08-13 15:32:32 +0530

    add

--- brigade.js
+++ brigade.js
@@ -83,7 +83,7 @@ job2.tasks = [
     "docker login -u shaxxz13 -p shubhu9797",
     "figlet Docker login",
     "docker build -t gcr.io/fluted-bit-244912/shaxxz13/shubhuxx . ",
-    "docker push gcr.io/fluted-bit-244912/shaxxz13/shubhuxx",
+    "gcloud docker push gcr.io/fluted-bit-244912/shaxxz13/shubhuxx",
     "figlet Image Push Successful"
     
 
