42aa7d4e9cfc609eadce6852138ceb55fc71d031
idadmin <idadmin@lapr169.successivetech.com>
2019-08-13 12:08:51 +0530

    test

--- brigade.js
+++ brigade.js
@@ -74,6 +74,7 @@ job2.tasks = [
     "gcloud components update",
     "gcloud auth activate-service-account 200715180751-compute@developer.gserviceaccount.com --key-file=/mnt/brigade/share/keys.json --project=fluted-bit-244912",
     "ls -lart",
+    "kubectl version",
     "gcloud container clusters get-credentials maincluster --zone us-central1-a --project fluted-bit-244912",
     "ls -lart",
 
