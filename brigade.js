2c0c7ef204caddcabece6b016f907cec595034a7
Shubhankar <shubhankar.kalra@successive.tech>
2019-08-19 11:36:39 +0530

    add

--- brigade.js
+++ brigade.js
@@ -85,11 +85,7 @@ job2.tasks = [
     "figlet CONFIG",
     "docker login -u shaxxz13 -p shubhu9797",
     "figlet Docker login",
-    "kubectl get serviceaccounts",
     "helm init --history-max 200",
-    // "kubectl create serviceaccount --namespace kube-system tiller",
-    // "kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller",
-    // "helm ls",
       //"docker build -t gcr.io/fluted-bit-244912/shaxxz13/shubhuxx . ",
       // "docker push gcr.io/fluted-bit-244912/shaxxz13/shubhuxx",
     "figlet Image Push Successful"
